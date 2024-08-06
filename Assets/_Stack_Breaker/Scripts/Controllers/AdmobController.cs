fe433a4b086bfb8af03959fa9132ef1628032483
ProC <hieutg244@gmail.com>
2024-08-06 20:47:59 +0700

    init

---
+++ Assets/_Stack_Breaker/Scripts/Controllers/AdmobController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+
+namespace CBGames
+{
+    public class AdmobController : MonoBehaviour
+    {
+        private void Awake()
+        {
+        }
+
+        private void Start()
+        {
+        }
+
+
+        /// <summary>
+        /// Hide the current banner ad
+        /// </summary>
+        public void HideBanner()
+        {
+        }
+
+
+        /// <summary>
+        /// Load and show a banner ad
+        /// </summary>
+        /// <summary>
+        /// Request interstitial ad
+        /// </summary>
+        public void RequestInterstitial()
+        {
+        }
+
+        /// <summary>
+        /// Request rewarded video ad
+        /// </summary>
+        public void RequestRewardedVideo()
+        {
+        }
+
+        /// <summary>
+        /// Determine whether the interstitial ad is ready
+        /// </summary>
+        /// <returns></returns>
+        public bool IsInterstitialReady() => true;
+
+        /// <summary>
+        /// Show interstitial ad with given delay time
+        /// </summary>
+        /// <param name="delay"></param>
+        public void ShowInterstitial(float delay)
+        {
+        }
+
+        /// <summary>
+        /// Determine whether the rewarded video ad is ready
+        /// </summary>
+        /// <returns></returns>
+        public bool IsRewardedVideoReady()
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// Show rewarded video ad with given delay time 
+        /// </summary>
+        /// <param name="delay"></param>
+        public void ShowRewardedVideo(float delay)
+        {
+        }
+
+        //Events callback
+        public void LoadAndShowBanner(int i)
+        {
+            
+        }
+    }
+}
